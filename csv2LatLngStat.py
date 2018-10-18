add402c26b1248fb9964f0b6485e684333434917
Kohei Matsumoto <matsumoto@irvs.ait.kyushu-u.ac.jp>
2018-10-18 22:53:03 +0900

    Make script

---
+++ csv2LatLngStat.py
@@ -0,0 +1,31 @@
+import csv
+import sys
+
+args = sys.argv
+
+num_lines = len(open(args[1]).readlines()) - 1
+
+with open('outLatLngStat.txt', 'w') as f_out:
+    with open(str(args[1]), 'r') as f_csv:
+        reader = csv.reader(f_csv)
+        header = next(reader)
+
+        f_out.write("var markerData = [" + "\n")
+
+        n = 1
+
+        for row in reader:
+
+            print(row)
+
+            f_out.write(" " * 18)
+            f_out.write("{" + "lat: " + row[2] + ", " + "lng: " + row[1] + ", " + "stat: " + row[0] + "}")
+            if n != num_lines:
+                f_out.write("," + "\n")
+            else:
+                f_out.write("\n")
+
+            n += 1
+
+    f_out.write(" " * 18)
+    f_out.write("];")
